813ea166276aab3c3972b24906f159bc9303945b
soufianechaghil <50269765+soufianechaghil@users.noreply.github.com>
2019-06-28 17:07:02 +0200

    Add files via upload

---
+++ page1.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<title>mon site &#8211; nouveausite</title>
+<link rel='dns-prefetch' href='//fonts.googleapis.com' />
+<link rel='dns-prefetch' href='//s.w.org' />
+<link rel="alternate" type="application/rss+xml" title="mon site &raquo; Flux" href="http://localhost/wordpress/feed/" />
+<link rel="alternate" type="application/rss+xml" title="mon site &raquo; Flux des commentaires" href="http://localhost/wordpress/comments/feed/" />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11.2.0\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11.2.0\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/localhost\/wordpress\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.1"}};
+			!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55358,56760,9792,65039],[55358,56760,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+	<link rel='stylesheet' id='dashicons-css'  href='http://localhost/wordpress/wp-includes/css/dashicons.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='admin-bar-css'  href='http://localhost/wordpress/wp-includes/css/admin-bar.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='wp-block-library-css'  href='http://localhost/wordpress/wp-includes/css/dist/block-library/style.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='boostrap-css-css'  href='http://localhost/wordpress/wp-content/themes/lawyeriax-lite/css/bootstrap.min.css?ver=v3.3.6' type='text/css' media='all' />
+<link rel='stylesheet' id='lawyeriax-style-css'  href='http://localhost/wordpress/wp-content/themes/lawyeriax-lite/style.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='font-awesome-css'  href='http://localhost/wordpress/wp-content/themes/lawyeriax-lite/css/font-awesome.min.css?ver=v4.5.0' type='text/css' media='' />
+<link rel='stylesheet' id='lawyeriax-fonts-css'  href='https://fonts.googleapis.com/css?family=Playfair+Display%3A400%2C700%7COpen+Sans%3A400%2C300%2C600%2C700&#038;subset=latin%2Clatin-ext' type='text/css' media='all' />
+<script type='text/javascript' src='http://localhost/wordpress/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='http://localhost/wordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<link rel='https://api.w.org/' href='http://localhost/wordpress/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://localhost/wordpress/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://localhost/wordpress/wp-includes/wlwmanifest.xml" /> 
+<meta name="generator" content="WordPress 5.1" />
+		<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+		<style type="text/css" media="print">#wpadminbar { display:none; }</style>
+	<style type="text/css" media="screen">
+	html { margin-top: 32px !important; }
+	* html body { margin-top: 32px !important; }
+	@media screen and ( max-width: 782px ) {
+		html { margin-top: 46px !important; }
+		* html body { margin-top: 46px !important; }
+	}
+	#a{
+		position : relative;
+		text-align : center;
+	}
+	#b{
+		position : relative;
+		border : 20px solid white;
+	}
+	#d{
+		text-align : center;
+		border : 20px solid white;
+	}
+	#c , #e {
+		font-size : 60px;
+		color : #FD6;
+	}
+</style>
+	</head>
+
+<body class="home blog logged-in admin-bar no-customize-support hfeed">
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content">Aller au contenu</a>
+
+		<header id="masthead" class="site-header" role="banner">
+
+			<div class="navbar navbar-main sticky-navigation navbar-fixed-top">
+				
+				<div class="container container-header">
+					<div class="header-inner">
+
+						<div class="header-inner-site-branding">
+							<div class="site-branding-wrap">
+								<div class="site-branding">
+																			<h1 class="site-title"><a href="http://localhost/wordpress/" rel="home">mon site</a></h1>
+								        											<p class="site-description">nouveausite</p>
+																		</div><!-- .site-branding -->
+							</div><!-- .site-branding-wrap -->
+
+							<div class="menu-toggle-button-wrap">
+								<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+									<i class="fa fa-bars"></i>
+									<span>Menu principal</span>
+								</button>
+							</div>
+						</div>
+
+						<div class="main-navigation-wrap">
+							<nav id="site-navigation" class="main-navigation" role="navigation">
+								<div id="primary-menu" class="menu"><ul>
+<li class="page_item page-item-2"><a href="http://localhost/wordpress/page-d-exemple/">Page d’exemple</a></li>
+</ul></div>
+							</nav><!-- #site-navigation -->
+						</div>
+					</div><!-- .header-inner -->
+				</div><!-- .container -->
+			</div>
+		</header><!-- #masthead -->
+
+		<div id="content" class="site-content">
+			<div class="container">
+
+	<div class="content-wrap">
+
+		<div id="primary" class="col-sm-12 col-md-9 content-area">
+			<main id="main" class="site-main" role="main">
+
+			
+<section class="no-results not-found">
+	<header class="page-header">
+		<!--<h1 class="page-title">Rien de trouvé</h1>-->
+	</header><!-- .page-header -->
+
+	<div class="page-content">
+		
+			
+			 
+			 
+	    <div id="a">
+		    <nav id="b">
+			<a href="connexion1.php"><button id="c">Pour Se connecter</button></a>
+			</nav>
+            <article id="d">
+			<a href="Inscription1.php"><button id="e">Pour S'inscrire</button></a>
+            </article>			
+		</div>
+			
+
+			</div><!-- .page-content -->
+</section><!-- .no-results -->
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+		<div class="section-line-sidebar">
+	<div class="clearfix"></div><div class="col-sm-10 col-sm-offset-1 section-line"></div><div class="clearfix"></div>
+</div>
+
+<aside id="secondary" class="col-sm-12 col-md-3 border-left widget-area" role="complementary">
+	<section id="search-2" class="widget widget_search"><form role="search" method="get" class="search-form" action="http://localhost/wordpress/">
+				<!--<label>
+					<span class="screen-reader-text">Rechercher :</span>
+					<input type="search" class="search-field" placeholder="Recherche&hellip;" value="" name="s" />
+				</label>
+				<input type="submit" class="search-submit" value="Rechercher" />-->
+			</form></section><section id="recent-comments-2" class="widget widget_recent_comments"><h2 class="widget-title">Lire a propos de nous</h2><ul id="recentcomments"></ul></section><section id="archives-2" class="widget widget_archive"><h2 class="widget-title">Nos services</h2>		<ul>
+					</ul>
+			<!--</section><section id="categories-2" class="widget widget_categories"><h2 class="widget-title">Catégories</h2>		<ul>
+			<li class="cat-item-none">Aucune catégorie</li>		</ul>
+			</section><section id="meta-2" class="widget widget_meta"><h2 class="widget-title">Méta</h2>			<ul>
+			<li><a href="http://localhost/wordpress/wp-admin/">Admin. du site</a></li>			<li><a href="http://localhost/wordpress/wp-login.php?action=logout&#038;_wpnonce=96d27ab160">Déconnexion</a></li>
+			<li><a href="http://localhost/wordpress/feed/">Flux <abbr title="Really Simple Syndication">RSS</abbr> des articles</a></li>
+			<li><a href="http://localhost/wordpress/comments/feed/"><abbr title="Really Simple Syndication">RSS</abbr> des commentaires</a></li>
+			<li><a href="https://wpfr.net" title="Propulsé par WordPress, plate-forme de publication personnelle sémantique de pointe.">Site de WordPress-FR</a></li>			</ul>
+			</section></aside><!-- #secondary -->
+
+	</div><!-- .content-wrap -->
+
+		</div><!-- .container -->
+	</div><!-- #content -->
+
+			<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="container">
+				<div class="col-sm-4">
+			        			    </div>
+
+				<div class="col-sm-4">
+			        			    </div>
+			    <div class="col-sm-4">
+				    			    </div>
+			</div><!-- .container -->
+
+			<div class="container">
+				<div class="site-info">
+					
+					<div class="site-info-inner">
+
+						<a href="https://themeisle.com/themes/lawyeriax-lite/" target="_blank" rel="nofollow">LawyeriaX Lite</a> powered by <a class="" href="http://wordpress.org/" target="_blank" rel="nofollow">WordPress</a>
+					</div><!-- .site-info-inner -->
+
+				</div><!-- .site-info -->
+			</div>
+		</footer><!-- #colophon -->
+	
+	
+		<div class="preloader">
+			<div class="status">&nbsp;</div>
+		</div>
+
+	
+
+</div><!-- #page -->
+
+<!-- Modal -->
+<div class="modal fade" id="siteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<button type="button" class="close modal-close-button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<div class="modal-content">
+			<div class="modal-body">
+
+				[pirate_forms]
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script type='text/javascript' src='http://localhost/wordpress/wp-includes/js/admin-bar.min.js?ver=5.1'></script>
+<script type='text/javascript' src='http://localhost/wordpress/wp-content/themes/lawyeriax-lite/js/bootstrap.min.js?ver=20130115'></script>
+<script type='text/javascript' src='http://localhost/wordpress/wp-content/themes/lawyeriax-lite/js/functions.js?ver=20120206'></script>
+<script type='text/javascript' src='http://localhost/wordpress/wp-content/themes/lawyeriax-lite/js/skip-link-focus-fix.js?ver=20130115'></script>
+<script type='text/javascript' src='http://localhost/wordpress/wp-includes/js/wp-embed.min.js?ver=5.1'></script>
+	<!--[if lte IE 8]>
+		<script type="text/javascript">
+			document.body.className = document.body.className.replace( /(^|\s)(no-)?customize-support(?=\s|$)/, '' ) + ' no-customize-support';
+		</script>
+	<![endif]-->
+	<!--[if gte IE 9]><!-->
+		<script type="text/javascript">
+			(function() {
+				var request, b = document.body, c = 'className', cs = 'customize-support', rcs = new RegExp('(^|\\s+)(no-)?'+cs+'(\\s+|$)');
+
+						request = true;
+		
+				b[c] = b[c].replace( rcs, ' ' );
+				// The customizer requires postMessage and CORS (if the site is cross domain)
+				b[c] += ( window.postMessage && request ? ' ' : ' no-' ) + cs;
+			}());
+		</script>
+	<!--<![endif]-->
+			<!--<div id="wpadminbar" class="nojq nojs">
+							<a class="screen-reader-shortcut" href="#wp-toolbar" tabindex="1">Aller à la barre d’outils</a>
+						<div class="quicklinks" id="wp-toolbar" role="navigation" aria-label="Barre d’outils">
+				<ul id='wp-admin-bar-root-default' class="ab-top-menu"><li id='wp-admin-bar-wp-logo' class="menupop"><a class='ab-item' aria-haspopup="true" href='http://localhost/wordpress/wp-admin/about.php'><span class="ab-icon"></span><span class="screen-reader-text">À propos de WordPress</span></a><div class="ab-sub-wrapper"><ul id='wp-admin-bar-wp-logo-default' class="ab-submenu"><li id='wp-admin-bar-about'><a class='ab-item' href='http://localhost/wordpress/wp-admin/about.php'>À propos de WordPress</a></li></ul><ul id='wp-admin-bar-wp-logo-external' class="ab-sub-secondary ab-submenu"><li id='wp-admin-bar-wporg'><a class='ab-item' href='https://wpfr.net'>Site de WordPress-FR</a></li><li id='wp-admin-bar-documentation'><a class='ab-item' href='https://codex.wordpress.org/'>Documentation</a></li><li id='wp-admin-bar-support-forums'><a class='ab-item' href='https://wpfr.net/support/'>Forums d’entraide</a></li><li id='wp-admin-bar-feedback'><a class='ab-item' href='https://wordpress.org/support/forum/requests-and-feedback'>Remarque</a></li></ul></div></li><li id='wp-admin-bar-site-name' class="menupop"><a class='ab-item' aria-haspopup="true" href='http://localhost/wordpress/wp-admin/'>mon site</a><div class="ab-sub-wrapper"><ul id='wp-admin-bar-site-name-default' class="ab-submenu"><li id='wp-admin-bar-dashboard'><a class='ab-item' href='http://localhost/wordpress/wp-admin/'>Tableau de bord</a></li></ul><ul id='wp-admin-bar-appearance' class="ab-submenu"><li id='wp-admin-bar-themes'><a class='ab-item' href='http://localhost/wordpress/wp-admin/themes.php'>Thèmes</a></li><li id='wp-admin-bar-widgets'><a class='ab-item' href='http://localhost/wordpress/wp-admin/widgets.php'>Widgets</a></li><li id='wp-admin-bar-menus'><a class='ab-item' href='http://localhost/wordpress/wp-admin/nav-menus.php'>Menus</a></li><li id='wp-admin-bar-background' class="hide-if-customize"><a class='ab-item' href='http://localhost/wordpress/wp-admin/themes.php?page=custom-background'>Arrière-plan</a></li><li id='wp-admin-bar-header' class="hide-if-customize"><a class='ab-item' href='http://localhost/wordpress/wp-admin/themes.php?page=custom-header'>En-tête</a></li></ul></div></li><li id='wp-admin-bar-customize' class="hide-if-no-customize"><a class='ab-item' href='http://localhost/wordpress/wp-admin/customize.php?url=http%3A%2F%2Flocalhost%2Fwordpress%2F'>Personnaliser</a></li><li id='wp-admin-bar-comments'><a class='ab-item' href='http://localhost/wordpress/wp-admin/edit-comments.php'><span class="ab-icon"></span><span class="ab-label awaiting-mod pending-count count-0" aria-hidden="true">0</span><span class="screen-reader-text">0 commentaire en attente de modération</span></a></li><li id='wp-admin-bar-new-content' class="menupop"><a class='ab-item' aria-haspopup="true" href='http://localhost/wordpress/wp-admin/post-new.php'><span class="ab-icon"></span><span class="ab-label">Créer</span></a><div class="ab-sub-wrapper"><ul id='wp-admin-bar-new-content-default' class="ab-submenu"><li id='wp-admin-bar-new-post'><a class='ab-item' href='http://localhost/wordpress/wp-admin/post-new.php'>Article</a></li><li id='wp-admin-bar-new-media'><a class='ab-item' href='http://localhost/wordpress/wp-admin/media-new.php'>Fichier média</a></li><li id='wp-admin-bar-new-page'><a class='ab-item' href='http://localhost/wordpress/wp-admin/post-new.php?post_type=page'>Page</a></li><li id='wp-admin-bar-new-user'><a class='ab-item' href='http://localhost/wordpress/wp-admin/user-new.php'>Utilisateur</a></li></ul></div></li></ul><ul id='wp-admin-bar-top-secondary' class="ab-top-secondary ab-top-menu"><li id='wp-admin-bar-search' class="admin-bar-search"><div class="ab-item ab-empty-item" tabindex="-1"><form action="http://localhost/wordpress/" method="get" id="adminbarsearch"><input class="adminbar-input" name="s" id="adminbar-search" type="text" value="" maxlength="150" /><label for="adminbar-search" class="screen-reader-text">Recherche</label><input type="submit" class="adminbar-button" value="Recherche"/></form></div></li><li id='wp-admin-bar-my-account' class="menupop with-avatar"><a class='ab-item' aria-haspopup="true" href='http://localhost/wordpress/wp-admin/profile.php'>Bonjour, <span class="display-name">admin</span><img alt='' src='http://0.gravatar.com/avatar/914f7994b0347a0d6c79c0ee8e5692d8?s=26&#038;d=mm&#038;r=g' srcset='http://0.gravatar.com/avatar/914f7994b0347a0d6c79c0ee8e5692d8?s=52&#038;d=mm&#038;r=g 2x' class='avatar avatar-26 photo' height='26' width='26' /></a><div class="ab-sub-wrapper"><ul id='wp-admin-bar-user-actions' class="ab-submenu"><li id='wp-admin-bar-user-info'><a class='ab-item' tabindex="-1" href='http://localhost/wordpress/wp-admin/profile.php'><img alt='' src='http://0.gravatar.com/avatar/914f7994b0347a0d6c79c0ee8e5692d8?s=64&#038;d=mm&#038;r=g' srcset='http://0.gravatar.com/avatar/914f7994b0347a0d6c79c0ee8e5692d8?s=128&#038;d=mm&#038;r=g 2x' class='avatar avatar-64 photo' height='64' width='64' /><span class='display-name'>admin</span></a></li><li id='wp-admin-bar-edit-profile'><a class='ab-item' href='http://localhost/wordpress/wp-admin/profile.php'>Modifier mon profil</a></li><li id='wp-admin-bar-logout'><a class='ab-item' href='http://localhost/wordpress/wp-login.php?action=logout&#038;_wpnonce=96d27ab160'>Se déconnecter</a></li></ul></div></li></ul>			</div>
+						<a class="screen-reader-shortcut" href="http://localhost/wordpress/wp-login.php?action=logout&#038;_wpnonce=96d27ab160">Se déconnecter</a>
+					</div>-->
+
+		
+</body>
+</html>
